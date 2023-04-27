0450bdc89d2e4e95507eb7238a2ccff9f2a77d4d
Muskan Dawar <sanchitdawar34@gmail.com>
2023-04-27 16:47:37 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import { useContext, useEffect } from "react";
+import { AppContext } from "./context/AppContext";
+import Home from './pages/Home'
+import BlogPage from './pages/BlogPage'
+import TagPage from './pages/TagPage'
+import CategoryPage from './pages/CategoryPage'
+import { Routes, Route, useSearchParams, useLocation } from "react-router-dom";
+
+
+export default function App() {
+  const {fetchBlogPosts} = useContext(AppContext)
+  const [searchParams, setSearchParams] = useSearchParams()
+  const location = useLocation()
+
+  useEffect(() => {
+    const page = searchParams.get('page') ?? 1;
+
+    if(location.pathname.includes('tags')){
+      //that means tag page needs to be shown
+      const tag = location.pathname.split('/').at(-1).replace("-", " ");
+      fetchBlogPosts(Number(page), tag)
+    }
+    else if(location.pathname.includes('categories')){
+      const category = location.pathname.split('/').at(-1).replaceAll('-', ' ')
+      fetchBlogPosts(Number(page), null, category)
+    }
+    else{
+      fetchBlogPosts(Number(page))
+    }
+  }, [location.pathname, location.search]); 
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/blog/:blogId" element={<BlogPage />}/>
+      <Route path="/tags/:tag" element={<TagPage />}/>
+      <Route path="/categories/:category" element={<CategoryPage />}/>
+    </Routes>
+  );
+}
